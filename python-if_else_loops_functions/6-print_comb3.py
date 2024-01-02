c00c130e5948d05ac1f4ac37c903b01ff8912944
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-02 16:28:12 -0600

    changed variable names

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for l in range(8):
-    for r in range(l, 10):
-        if l != r:
-            print("{}{}".format(l, r), end=", ")
+for tens in range(8):
+    for ones in range(tens, 10):
+        if tens != ones:
+            print("{}{}".format(tens, ones), end=", ")
 print("89")
