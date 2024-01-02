a78346e82b5f798a75a4502fa3bd033d40baf886
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-02 14:00:42 -0600

    First attempt at 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("obj"): str.find("lang")]\
-	  + str[str.find("wi"):str.find("very")]\
-		  + str[:str.find("is")]
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
