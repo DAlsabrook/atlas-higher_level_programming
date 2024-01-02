0baa23b4d395ee633b7ac6c1e1181c60978be2f6
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-02 15:16:49 -0600

    Handling negative digits

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
+if number < 0:
+    last = -abs(last)
 string = "and is greater than 5"
 if last == 0:
     string = "and is 0"
