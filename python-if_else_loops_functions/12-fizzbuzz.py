df3e14e9a5b05a992bae00cd35a78697b659b97d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-03 12:47:08 -0600

    extended range to include 100

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         print("FizzBuzz" if i % 3 == 0 and i % 5 == 0
               else "Fizz" if i % 3 == 0
               else "Buzz" if i % 5 == 0
