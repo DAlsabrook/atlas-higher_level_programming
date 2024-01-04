73bf684a8e6627d03941fea8a26d48afdbe3f873
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-03 19:22:26 -0600

    changed variable name to avoid shadowing

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 
 if __name__ == "__main__":
     import sys
-    len = len(sys.argv)
-    if len == 2:
-        print("{} argument:\n1: {}".format(len - 1, sys.argv[1]))
-    elif len > 2:
-        print("{} arguments:".format(len - 1))
-        for arg in range(1, len):
+    arg_len = len(sys.argv)
+    if arg_len == 2:
+        print("{} argument:\n1: {}".format(arg_len - 1, sys.argv[1]))
+    elif arg_len > 2:
+        print("{} arguments:".format(arg_len - 1))
+        for arg in range(1, arg_len):
             print("{}: {}".format(arg, sys.argv[arg]))
     else:
         print(".")
