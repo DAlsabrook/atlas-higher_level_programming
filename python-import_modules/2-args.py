9bd7b2eb5e3c134577e4a16d7b76085d1ea6a1ed
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-03 20:17:52 -0600

    Redo on task 2 for 0 args case

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 
 if __name__ == "__main__":
     import sys
-    arg_len = len(sys.argv)
-    if arg_len == 2:
-        print("{} argument:\n1: {}".format(arg_len - 1, sys.argv[1]))
-    elif arg_len > 2:
-        print("{} arguments:".format(arg_len - 1))
-        for arg in range(1, arg_len):
-            print("{}: {}".format(arg, sys.argv[arg]))
+    arg_len = len(sys.argv) - 1
+    if arg_len == 1:
+        print("1 argument:")
+    elif arg_len > 1:
+        print("{} arguments:".format(arg_len))
     else:
-        print(".")
+        print("0 arguments.")
+    for arg in range(1, arg_len + 1):
+        print("{}: {}".format(arg, sys.argv[arg]))
