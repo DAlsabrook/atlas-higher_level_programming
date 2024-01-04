dc83070054073f621486374406e62e263a9e6515
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-03 19:04:00 -0600

    Guessing on the last check. idk what it needs

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-import sys
-len = len(sys.argv)
-
 if __name__ == "__main__":
+    import sys
+    len = len(sys.argv)
     if len == 2:
         print("{} argument:\n1: {}".format(len - 1, sys.argv[1]))
     elif len > 2:
