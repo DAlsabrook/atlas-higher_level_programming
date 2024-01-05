258dab74f37b32126da507eff796ceebfcfb7a33
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-05 13:54:44 -0600

    Changed idx < 0

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     if my_list is None or element is None:
         return None
-    if idx <= 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list):
         return my_list
     new_list = my_list.copy()
     new_list[idx] = element
