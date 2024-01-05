358ba8076f916b2f9092daba0fbac7f469327217
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-05 09:55:22 -0600

    Added :d format specifier to the string

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,5 +3,4 @@
 if __name__ == "__main__":
     def print_list_integer(my_list=[]):
         for nums in range(len(my_list)):
-            print("{}".format(my_list[nums]))
-
+            print("{:d}".format(my_list[nums]))
