6707ba56a1749adb12b39f072d152ae647effc07
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-06 13:19:38 -0600

    if statement to handle adding spaces

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for num in list:
             print("{:d}".format(num), end="")
-            if num != len(list):
+            if num != list[-1]:
                 print(" ", end="")
         print("")
