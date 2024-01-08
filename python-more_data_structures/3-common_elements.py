923f68d955b02a4ec58b5aa774e7671104c65fe5
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 13:49:11 -0600

    return nothing

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,7 +2,7 @@
 
 def common_elements(set_1, set_2):
     if set_1 is None or not set_1 or set_2 is None or not set_2:
-        return None
+        return
     common_elements = set()
     for element in set_1:
         for item in set_2:
