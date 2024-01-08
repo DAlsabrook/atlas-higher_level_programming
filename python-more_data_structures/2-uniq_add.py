afca441022be6ad345f1aaa134bb3e5da5de58fc
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 12:54:12 -0600

    First attempt at 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    
+    if not my_list:
+        return my_list
+    result = 0
+    tmp_list = set(my_list)
+    for number in tmp_list:
+        result += number
+    return result
