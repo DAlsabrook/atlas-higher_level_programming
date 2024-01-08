2a3dee5b9cef8c206e5c0fb91b616d0b5c397e7f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-07 22:50:43 -0600

    Error handling

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     if my_list is None or not my_list:
-        return None
+        return my_list
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     del my_list[idx]
