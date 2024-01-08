f697e7c555f9f94a995cf502ec729b03ec2f3dc5
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 17:10:18 -0600

    return dict

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,7 @@
 
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary is None or not a_dictionary:
-        return
+        a_dictionary = {}
     if key is not None:
         a_dictionary.update({key: value})
+    return a_dictionary
