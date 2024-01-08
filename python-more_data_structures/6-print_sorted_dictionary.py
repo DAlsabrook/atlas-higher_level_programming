8b2c177421b4df2ab98fb0812dca27b7ba26b2f0
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 14:46:06 -0600

    returning as tuples

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
-        print(key, value)
+    return sorted(a_dictionary.items())
