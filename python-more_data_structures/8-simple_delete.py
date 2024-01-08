4c27f4f24a6d19046f8829eb784a82245ad9b35f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 17:21:03 -0600

    delete if found in dict

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -3,6 +3,6 @@
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is None or not a_dictionary:
         a_dictionary = {}
-    if key is not None:
+    if key is not None and key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
