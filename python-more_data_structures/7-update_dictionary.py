800a68e9def841b581dee0279cdf2d8690584ba0
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 15:42:41 -0600

    First attempt at task 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is None or not a_dictionary:
+        return
+    if key is not None:
+        a_dictionary.update({key: value})
