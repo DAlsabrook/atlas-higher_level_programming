24cbdd77606bb68be010f0005c03d676d82e4f2c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 13:47:07 -0600

    changed to .add and declaration of set()

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -3,10 +3,10 @@
 def common_elements(set_1, set_2):
     if set_1 is None or not set_1 or set_2 is None or not set_2:
         return None
-    common_elements = {}
+    common_elements = set()
     for element in set_1:
         for item in set_2:
             if element == item:
-                common_elements.append(element)
+                common_elements.add(element)
     return common_elements
 
