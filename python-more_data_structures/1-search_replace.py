69f30f95e46cfffeb3dc704d8868822914b54f91
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 11:25:09 -0600

    Swwitched to using list comprehention instead of external loop and if statement

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,10 +3,4 @@
 def search_replace(my_list, search, replace):
     if my_list is None or not my_list:
         return my_list
-    new_list = my_list
-    for idx, item in enumerate(my_list):
-        if item == search:
-            new_list[idx] = replace
-        else:
-            new_list[idx] = item
-    return new_list
+    return [replace if item == search else item for item in my_list]
