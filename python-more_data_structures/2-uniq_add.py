59c9b14471757e8fc8162fafa80463530519373b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 15:03:59 -0600

    remove check for null

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list is None or not my_list:
-        return my_list
     uniq_ints = set()
     for nums in my_list:
         if isinstance(nums, int):
