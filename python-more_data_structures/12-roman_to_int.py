7e138ed4b8d389dc831cb48c925733732e3ea32f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 14:13:31 -0600

    switching to prev_num > to assign subtraction. Handles cases for I's after a number

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,30 @@
 #!/usr/bin/python
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    result = 0
+    prev_num = 0
+
+    for char in reversed(roman_string):
+        num = roman_numerals.get(char, 0)
+
+        if prev_num > num:
+            result -= num
+        else:
+            result += num
+
+        prev_num = num
+
+    return result
+        
