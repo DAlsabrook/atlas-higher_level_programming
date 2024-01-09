efcc75dc2013083df8a850346e024567dfeae706
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 14:55:34 -0600

    moved not my_list to return

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if not my_list:
-        return my_list
     result = sum(set(my_list))
-    return result
+    return result if my_list else my_list
