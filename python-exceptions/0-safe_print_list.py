1a61645c847eb5f84ba60c2c2c4d4e2b800d616b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 17:09:27 -0600

    First attempt at 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    if my_list is None or not my_list:
+        return 0
+    count = 0
+    try:
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
