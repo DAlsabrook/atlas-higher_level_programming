15d38a59ce5cac6932483e2f0aeba7ebcb88e229
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 21:32:03 -0600

    removed debug print

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,6 @@ def best_score(a_dictionary):
     if a_dictionary is None or not a_dictionary:
         return
     res = next(iter(a_dictionary.values()))
-    print(res)
     for value in a_dictionary.values():
         if value > res:
             res = value 
