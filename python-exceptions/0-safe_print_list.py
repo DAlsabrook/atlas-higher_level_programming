68a5c4d61b4f3cc9a18c0e4f6ad5e1c7d4fa96d4
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 17:15:13 -0600

    changing to a while loop and count

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list is None or not my_list:
-        return 0
     count = 0
     try:
-        for i in range(x):
-            print(f"{my_list[i]}", end="")
+        while count <= x:
+            print(f"{my_list[count]}", end="")
             count += 1
     except IndexError:
         pass
