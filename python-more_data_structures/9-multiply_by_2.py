89d4450433a3f8591f2eac8aa4040d5ea08de0f2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-08 21:01:48 -0600

    First attempt at 9

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    if a_dictionary is None or not a_dictionary:
+        return
+    new_dict = a_dictionary.copy()
+    for key, value in new_dict.items():
+        new_dict[key] = value * 2
+    return new_dict
+
