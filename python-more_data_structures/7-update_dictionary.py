a504600c199218eda461a226e1cde1d55e3ce79a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 15:08:26 -0600

    Removed check for null

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is None or not a_dictionary:
-        a_dictionary = {}
     if key is not None:
         a_dictionary.update({key: value})
     return a_dictionary
