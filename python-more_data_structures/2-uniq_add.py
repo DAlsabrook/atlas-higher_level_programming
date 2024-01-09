fe68f4e621f759aec41b6aa5f356b42010c3807f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 15:03:14 -0600

    moving null check

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if my_list is None or not my_list:
+        return my_list
     uniq_ints = set()
     for nums in my_list:
         if isinstance(nums, int):
             uniq_ints.add(nums)
-    return sum(set(uniq_ints)) if my_list else my_list
+    return sum(set(uniq_ints))
