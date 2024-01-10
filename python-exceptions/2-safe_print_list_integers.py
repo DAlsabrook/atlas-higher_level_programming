dfccbfe05231649cbeaa323df3c40d833332174c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 18:34:04 -0600

    First attempt at task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nums_printed = 0
+    try:
+        for num in range(x):
+            if type(my_list[num]) == int:
+                print("{}".format(my_list[num]), end="")
+                nums_printed += 1
+    except IndexError:
+        pass
+    print()
+    return nums_printed
