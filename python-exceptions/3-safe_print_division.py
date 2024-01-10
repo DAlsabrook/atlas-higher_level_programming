a1e8b2e719afae9681d1ff9be29f16fe3f0834e8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 20:38:10 -0600

    Moved res declaration

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    res = None
     try:
         res = float(a // b)
     except (ZeroDivisionError, TypeError):
-        res = None
+        pass
     finally:
         print("Inside result: {}". format(res))
     return res
