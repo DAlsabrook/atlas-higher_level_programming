4ed4c057b654d73233d3f52f263bf6d4f3c08634
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 18:57:41 -0600

    removed tuple brackets

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[nums]), end="")
             nums_printed += 1
-        except (ValueError):
+        except ValueError:
             continue
     print()
     return nums_printed
