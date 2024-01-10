2239b7cad916cd65e9cf148f9e624a6473d051d3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 18:51:52 -0600

    try inside of loop to continue with list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     nums_printed = 0
-    try:
-        for num in range(x):
-            if type(my_list[num]) == int:
-                print("{}".format(my_list[num]), end="")
-                nums_printed += 1
-    except IndexError:
-        pass
+    for nums in range(x):
+        try:
+            print("{:d}".format(my_list[nums]), end="")
+            nums_printed += 1
+        except (IndexError, ValueError):
+            continue
     print()
     return nums_printed
--- python-exceptions/test.py
+++ python-exceptions/test.py
@@ -13,11 +13,3 @@ print("nb_print: {:d}".format(nb_print))
 
 nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
-
-my_list = []
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, -2, 3, -4, 5]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
