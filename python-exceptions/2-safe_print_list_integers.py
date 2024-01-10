7281d76b58c96c4cc8a8df2f0b8f510700355d63
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 19:16:18 -0600

    Added type error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[nums]), end="")
             nums_printed += 1
-        except (Exception):
+        except (Exception, TypeError):
             continue
     print()
     return nums_printed
