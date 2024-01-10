fe9a94195c9fb8b950e09d79115a4e6201b0bbac
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 20:39:08 -0600

    Moves res back down to except

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res = None
     try:
         res = float(a // b)
     except (ZeroDivisionError, TypeError):
-        pass
+        res = None
     finally:
         print("Inside result: {}". format(res))
     return res
