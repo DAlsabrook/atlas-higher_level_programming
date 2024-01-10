ff9e40e7a9bd353e945b8c87bbeb75739c96976a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 20:39:40 -0600

    Back to 1 error

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         res = float(a // b)
-    except (ZeroDivisionError, TypeError):
+    except (ZeroDivisionError):
         res = None
     finally:
         print("Inside result: {}". format(res))
