12b9f7c0c14526796c388b862f9ed29a0498bb3d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 20:31:27 -0600

    added type error

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         res = float(a // b)
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         res = None
     finally:
         print("Inside result: {}". format(res))
