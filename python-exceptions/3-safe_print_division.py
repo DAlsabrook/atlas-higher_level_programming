daf51f66f8a556c88b6b1505ed7752615b234de3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-09 20:32:34 -0600

    Added except as e

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,6 +4,8 @@ def safe_print_division(a, b):
         res = float(a // b)
     except (ZeroDivisionError, TypeError):
         res = None
+    except Exception as e:
+        print("Exception:", e)
     finally:
         print("Inside result: {}". format(res))
     return res
