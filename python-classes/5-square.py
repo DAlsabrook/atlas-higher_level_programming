e50838349c105b95769765690f501a34a0fc58dc
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-12 12:28:47 -0600

    First attempt at 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Definition of the square class"""
+
+class Square:
+    """attributes of the square class"""
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.__size = size
+    """Getter for the size attribute"""
+    @property
+    def size(self):
+        return self.__size
+
+    """Setter for size property"""
+    @size.setter
+    def size(self, value):
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        elif not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        self.__size = value
+
+    """Method that returns the square area of the square"""
+    def area(self):
+        return self.__size ** 2
+
+    """Method that prints the square using '#' char"""
+    def my_print(self):
+        for row in range(self.__size):
+            for element in range(self.__size):
+                print("#", end="")
+            print()
