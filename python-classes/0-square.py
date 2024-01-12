c290bf788d74e1e4bf2a6aacf1b60375a03dbba5
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-12 10:44:14 -0600

    Uppercase class name

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,7 @@
 """Class for defining a square as nothing to start"""
 
 
-class square:
+class Square:
     """Definition of a square object"""
     def __init__(self):
         """attributes of a square"""
