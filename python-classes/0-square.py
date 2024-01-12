d00469de16d1cf19139ee51d7031002b62c5b370
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-11 23:16:11 -0600

    Just pushing some practice for documentation and noting for tomorrow when i start project 1

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+'''Class documentation'''
+class square:
+    '''class variable documentation'''
+    size = 0
+    lenght = 0
+
+    '''Method documentation'''
+    def method(var):
+        '''attribute documentation'''
