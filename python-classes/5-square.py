dca7b79335be177340640d6b835afcffc3e3c4ec
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-12 12:32:16 -0600

    Added print case for if size == 0

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Definition of the square class"""
 
+
 class Square:
     """attributes of the square class"""
     def __init__(self, size=0):
@@ -29,6 +30,8 @@ class Square:
 
     """Method that prints the square using '#' char"""
     def my_print(self):
+        if self.__size == 0:
+            print()
         for row in range(self.__size):
             for element in range(self.__size):
                 print("#", end="")
