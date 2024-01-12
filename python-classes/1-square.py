0f92bbb3cb572c76da06d071b89b4a0faca5b0e2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-12 10:54:24 -0600

    First attempt at 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square is a class used to define a square object"""
+
+
+class Square:
+    """Attributes of square object"""
+    def __init__(self, size):
+        self.__size = size
