97256687f08cfd57f80d1b0f76f8e9fdfbed9d03
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-13 21:17:42 -0600

    Used unit test instead of doctest. removing unit test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""
+Function to add two integers together
+"""
+
 
 def add_integer(a, b=98):
-    """Function that adds two integers"""
+    """
+    Function for simple addition
+
+    Args:
+        a: first int to use
+        b: second int to use
+
+    Return: The result of addition
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
