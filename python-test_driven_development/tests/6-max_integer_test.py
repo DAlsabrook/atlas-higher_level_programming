51a5648abc440a39349ab7ba5bd60a19d917f04b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-15 17:18:20 -0600

    Rearanged and commented

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -12,25 +12,30 @@ max_integer = max_integer_module.max_integer
 
 class TestMaxInt(unittest.TestCase):
 
+    # Test for normal use case
     def test_max_int_normal(self):
         list = [1, 2, 3]
         self.assertEqual(max_integer(list), 3)
 
-    def test_max_int_negative(self):
-        list = [-1, -2, -3]
-        self.assertEqual(max_integer(list), -1)
-
+    # Test for max int being in the middle of the list
     def test_max_int_middle(self):
         list = [1, 10, 3]
         self.assertEqual(max_integer(list), 10)
 
-    def test_max_int_empty(self):
-        list = []
-        self.assertFalse(max_integer(list))
+    # Test for negative numbers and max being the first element
+    def test_max_int_negative(self):
+        list = [-1, -2, -3]
+        self.assertEqual(max_integer(list), -1)
 
+    # Test for a list of 1 element
     def test_max_int_one(self):
         list = [1]
         self.assertEqual(max_integer(list), 1)
+
+    # Test for an empty list
+    def test_max_int_empty(self):
+        list = []
+        self.assertFalse(max_integer(list))
 
 if __name__ == "__main__":
     unittest.main()
