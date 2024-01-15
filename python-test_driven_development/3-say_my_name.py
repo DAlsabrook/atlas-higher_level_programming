0184c6522153d3c2e8c7f711b4d5103153a2fdaf
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-15 14:16:12 -0600

    Missunderstood error handling

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -19,7 +19,8 @@ def say_my_name(first_name, last_name=""):
         Raises:
             TypeError: if first_name or last_name is not a str
     """
-    message = "first_name must be a string or last_name must be a string"
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError(message)
+    if not isinstance(first_name, str)
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print(f"My name is {first_name} {last_name}")
