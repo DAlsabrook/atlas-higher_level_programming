afda536e01ba71c76a8960a4b9dd9fcc4b570c53
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 12:24:16 -0600

    First attempt at 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module holds a function for finds if var is a child of class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function that finds if obj is the child or grandchild of a_class
+
+    Args:
+        obj (object): The object to use
+        a_class (class): Use to find if obj is a child or grandchild
+
+    Return:
+        True if related
+        False if not
+    """
+    return issubclass(a_class, obj)
