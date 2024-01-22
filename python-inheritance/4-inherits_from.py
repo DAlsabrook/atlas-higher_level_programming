6712c0760d69d670be675ac757657472b38e1f2c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 12:34:37 -0600

    First attempt at 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module to hold a function for finding if obj is inheriting
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function that finds if obj is inheriting from a_class
+
+    Args:
+        obj (object): Object to compare
+        a_class (class): Class to see if obj inherits from
+
+    Returns:
+        True if obj inherits
+        False if not
+    """
+    return issubclass(obj, a_class)
