562489370974d5f1067f5a22bd8b3c75ed883d00
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 15:47:57 -0600

    Remove newline from print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ Module for reading a file with with as
 def read_file(filename=""):
     """Method that reads a file and prints to stdout"""
     with open(filename) as file:
-        print(file.read())
+        print(file.read(), end="")
