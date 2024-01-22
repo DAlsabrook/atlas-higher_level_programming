79149908ce871043a51fc8188660e5dc43760489
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 15:46:56 -0600

    Adding documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+Module for reading a file with with as
+"""
 def read_file(filename=""):
+    """Method that reads a file and prints to stdout"""
     with open(filename) as file:
         print(file.read())
