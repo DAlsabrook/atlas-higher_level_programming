6650976c5796ba499354f339096dd6ab9ea28be1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 12:13:54 -0600

    First attempt at 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+"""
+Module for a function that looks for class type
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that compares if object is an instance of a class
+
+    Args:
+        obj (object): Object to compare
+        a_class (class): Class to compare against
+
+    Returns:
+        True if obj is the same class
+        False if obj if of a different class
+    """
+    return True if type(obj) == type(a_class) else False
