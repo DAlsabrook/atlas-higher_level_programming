39f7b6268cd7e3de7bb9ae3ddcb6da40709f7ec7
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 19:57:37 -0600

    First attempt at 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module to create obj from json file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Turn a JSON file into an object
+    """
+    with open(filename, "r") as file:
+        obj = json.loads(file)
+    return obj
