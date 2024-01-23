808b5bd9b383d29f5420932efd7509525f251ddd
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 19:44:54 -0600

    First atttempt at 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for converting object to json
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Method that converts obj to json
+    """
+    return json.dumps(my_obj)
