f2bcbcca030b608bb3933347065611d470e7487a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 19:59:51 -0600

    casting to str

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -10,5 +10,5 @@ def load_from_json_file(filename):
     Turn a JSON file into an object
     """
     with open(filename, "r") as file:
-        obj = json.loads(file)
+        obj = json.loads(str(file))
     return obj
