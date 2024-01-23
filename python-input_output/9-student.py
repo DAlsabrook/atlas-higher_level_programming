dddc8ebe12b376828fddd5e1ac4ce1519d3a382b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-23 12:38:14 -0600

    First attempt at 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module that contains a class for "student"
+"""
+import json
+
+
+class Student:
+    """
+    Class to define a student
+
+    Attributes:
+        first_name (str): Student first name
+        last_name (str): Student last name
+        age (int): Student age
+
+    Methods:
+        to_json(self): Return a dictionary representation of a Student instance
+
+    Raises:
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        dictionary = self.__dict__
+        return dictionary
