4b061731d2e64ed041715a7880a191e544fad612
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 19:50:58 -0600

    trying simpler version

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -3,11 +3,10 @@
 Module for converting from json to object
 """
 import json
-from types import SimpleNamespace
 
 
 def from_json_string(my_str):
     """
     Method to convert json into an object
     """
-    return json.loads(my_str, object_hook=lambda d: SimpleNamespace(**d))
+    return json.loads(my_str)
