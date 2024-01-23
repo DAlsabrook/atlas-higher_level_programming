ecfe3539fc3c4d4d72fa1135dd91733378d98ec1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 21:01:33 -0600

    Comment

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,6 +7,8 @@ Module to convert a class to JSON
 def class_to_json(obj):
     obj_dict = obj.__dict__
 
+    # looking to find if any value is a list aka has a __dict__ attribute
+    # and is a class
     for key, value in obj_dict.items():
         if hasattr(value, '__dict__'):
             obj_dict[key] = class_to_json(value)
