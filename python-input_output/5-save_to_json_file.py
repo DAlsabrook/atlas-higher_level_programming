69fdaf5ea2cefda42b5d621528837e4cce340db8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-22 19:54:47 -0600

    First attempt at 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module to save to json file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Method to write obj to a text file, using JSON representation
+    """
+    with open(filename, "a") as file:
+        return file.write(json.dumps(my_obj))
