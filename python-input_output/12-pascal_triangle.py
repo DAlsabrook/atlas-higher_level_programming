dd6f9c260c19857421b6b9e5ffca5e620f8e22c9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-23 14:53:55 -0600

    First attempt at 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module to contain a function for returning Pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    matrix = []
+    if n <= 0:
+        return []
+    for list in range(n):
+        tmp = []
+        if list == 0:
+            tmp = [1]
+            matrix.append(tmp)
+            continue
+        for element in range(len(matrix[list - 1]) + 1):
+            try:
+                x = matrix[list - 1][element]
+            except IndexError:
+                tmp.append(matrix[list - 1][element - 1])
+                continue
+            try:
+                if element == 0:
+                    tmp.append(1)
+                    continue
+                y = matrix[list - 1][element - 1]
+            except IndexError:
+                tmp.append(matrix[list - 1][element])
+                continue
+            tmp.append(x + y)
+        matrix.append(tmp)
