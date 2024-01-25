bdc91d663443dcb547357bc5a5cc6fa64ec2c4fb
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-25 12:36:58 -0600

    First attempt at 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module for the Base class to build on
+"""
+
+
+class Base:
+    """
+    Class to define a base id for subclasses
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if not id == None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module for unit test of the base class
+"""
+import unittest
+from ...models.base import Base
+
+
+class Base_test(unittest.TestCase):
+    """Class to run unit test on base class
+
+    Args:
+        unittest (object): class for running unit test
+    """
+
+    def test_none(self):
+        """Case for default None value given to class with no id given
+        """
+        x = Base()
+        self.assertEqual(x.id, 1)
+
+    def test_given_id(self):
+        """Case for id given as argument
+        """
+        x = Base(4)
+        self.assertEqual(x.id, 4)
+
+    def test_negative(self):
+        """Case for negative number given as id
+        """
+        x = Base(-1)
+        self.assertEqual(x.id, -1)
+
+    def test_float(self):
+        """Case for float given as id
+        """
+        x = Base(1.2)
+        self.assertEqual(x.id, 1.2)
