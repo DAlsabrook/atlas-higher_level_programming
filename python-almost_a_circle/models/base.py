f727d2ae2ab427c26eee1be2d6e95a0ad6fda150
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-25 12:38:05 -0600

    changed comparison to None to is not ==

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,7 +11,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if not id == None:
+        if not id is None:
             self.id = id
         else:
             Base.__nb_objects += 1
