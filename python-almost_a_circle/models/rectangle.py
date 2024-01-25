0a700d0ee73c19449ff16e1966b6f502144c9f28
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-25 13:37:37 -0600

    First attempt at 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Module to hold the class for a rectangle
+"""
+from .base import Base
+
+
+class Rectangle(Base):
+    """Class to define a rectangle
+
+    Args:
+        Base (object): used to define id
+
+    Attributes:
+        Width(int): how wide rect is
+        Height(int): How high rect is
+        x(int): idk
+        y(int): idk
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Module for unittest of the Rectangle class
+"""
+import unittest
+from ...models.rectangle import Rectangle
+from ...models.base import Base
+
+
+class Test_Rectangle(unittest.TestCase):
+    """Unit test for the Rectangle class
+
+        Rectangle(width, height, x, y, id)
+    """
+    def test_height(self):
+        """Height given
+        """
+        x = Rectangle(2, 4)
+        self.assertEqual(x.height, 4)
+
+    def test_width(self):
+        """Width given
+        """
+        x = Rectangle(2, 4)
+        self.assertEqual(x.width, 2)
+
+    def test_x_given(self):
+        """X given
+        """
+        x = Rectangle(2, 4, 6)
+        self.assertEqual(x.x, 6)
+
+    def test_y_given(self):
+        """Y given
+        """
+        x = Rectangle(2, 4, 6, 8)
+        self.assertEqual(x.y, 8)
+
+    def test_id_given(self):
+        """id Given
+        """
+        x = Rectangle(2, 4, 6, 8, 10)
+        self.assertEqual(x.id, 10)
+
+    def test_no_id(self):
+        """No id given
+        """
+        x = Rectangle(2, 4)
+        self.assertEqual(x.id, 4)
+
+    def test_multiple_id(self):
+        """Multiple instances for id
+        """
+        y = Rectangle(1, 3)
+        x = Rectangle(2, 4)
+        self.assertEqual(x.id, 3)
+
+
