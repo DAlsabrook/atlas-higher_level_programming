2aa9baa3fe38a96c0b01d7924c88161ec04b19f3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-26 11:21:06 -0600

    switching around assignments for update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -115,8 +115,8 @@ class Rectangle(Base):
     def update(self, *args):
         """give the caller the ability to update multiple attributes at once
         """
-        args[0] = self.id
-        args[1] = self.__width
-        args[2] = self.__height
-        args[3] = self.__x
-        args[4] = self.__y
+        self.id = args[0]
+        self.__width = args[1]
+        self.__height = args[2]
+        self.__x = args[3]
+        self.__y = args[4]
