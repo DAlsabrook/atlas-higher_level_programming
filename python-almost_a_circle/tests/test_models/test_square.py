8fdda76f1c6d7c6b370df9cb1ecba2309156ee8c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-26 13:22:50 -0600

    Added test file for square

---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module to test the square class
+"""
+from ...models.square import Square
+import unittest
+
+
+class Test_Square(unittest.TestCase):
+    """
+    Class to test the square class
+    """
+    
