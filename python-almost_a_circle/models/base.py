a7beab546fbed43c2704df1bbf6bc102c6408f3c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-30 12:18:53 -0600

    Added method to Base class to convert to JSON string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Module for the Base class to build on
 """
+import json
 
 
 class Base:
@@ -16,3 +17,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation of dict
+
+        Args:
+            list_dictionaries (list): list of dictionaries
+        """
+        if list_dictionaries is None or []:
+            return "[]"
+        return json.dumps(list_dictionaries)
