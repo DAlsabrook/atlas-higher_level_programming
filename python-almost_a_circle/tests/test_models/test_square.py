540818852ecc3197b7de7c417bba9e2b9bcda754
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-30 23:53:04 -0600

    to dict

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -13,10 +13,13 @@ class Test_Square(unittest.TestCase):
     def test_normal(self):
         self.assertEqual(Square(5).size, 5)
 
+    def test_zero(self):
+        with self.assertRaises(ValueError):
+            s = Square(0)
+
     def test_two_args(self):
         s1 = Square(10, 2)
-        s2 = Square(2, 10)
-        self.assertEqual(s1.id, s2.id - 1)
+        self.assertEqual(s1.id, 44)
 
     def test_three_args(self):
         s1 = Square(10, 2, 2)
@@ -30,6 +33,14 @@ class Test_Square(unittest.TestCase):
         with self.assertRaises(TypeError):
             s = Square("2")
 
+    def test_str_two(self):
+        with self.assertRaises(TypeError):
+            s = Square(1, "2")
+
+    def test_str_three(self):
+        with self.assertRaises(TypeError):
+            s = Square(1, 2, "2")
+
     def test_neg(self):
         with self.assertRaises(ValueError):
             s = Square(-2)
@@ -37,3 +48,20 @@ class Test_Square(unittest.TestCase):
     #area()
     def test_area_small(self):
         self.assertEqual(100, Square(10, 0, 0, 1).area())
+
+    #todictionary
+    def test_to_dictionary_output(self):
+        s = Square(10, 2, 1, 1)
+        correct = {'id': 1, 'x': 2, 'size': 10, 'y': 1}
+        self.assertDictEqual(correct, s.to_dictionary())
+
+    def test_to_dictionary_no_object_changes(self):
+        s1 = Square(10, 2, 1, 2)
+        s2 = Square(1, 2, 10)
+        s2.update(**s1.to_dictionary())
+        self.assertNotEqual(s1, s2)
+
+    def test_to_dictionary_arg(self):
+        s = Square(10, 10, 10, 10)
+        with self.assertRaises(TypeError):
+            s.to_dictionary(1)
