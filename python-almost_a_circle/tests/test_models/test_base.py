fa8e03b39edf551122021e80afff4bfe50db9071
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-01-30 21:31:09 -0600

    from a string method test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -13,7 +13,7 @@ class Test_Base(unittest.TestCase):
     Args:
         unittest (object): class for running unit test
     """
-
+    #Testing normal Base instances
     def test_empty(self):
         b1 = Base()
         b2 = Base()
@@ -33,6 +33,7 @@ class Test_Base(unittest.TestCase):
     def test_given_id(self):
         self.assertEqual(Base(4).id, 4)
 
+    #Testing to_json_string method
     def test_to_json_string(self):
         r = Rectangle(10, 7, 2, 8, 6)
         self.assertEqual(str, type(Base.to_json_string([r.to_dictionary()])))
@@ -43,5 +44,15 @@ class Test_Base(unittest.TestCase):
     def test_to_json_string_none(self):
         self.assertEqual("[]", Base.to_json_string(None))
 
-if __name__ == "__main__":
-    unittest.main()
+    #Testing from_json_string
+    def test_from_json_string_None(self):
+        self.assertEqual([], Base.from_json_string(None))
+
+    def test_from_json_string_empty_list(self):
+        self.assertEqual([], Base.from_json_string("[]"))
+
+    def test_from_json_string_one_rectangle(self):
+        list_input = [{"id": 89, "width": 10, "height": 4}]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_input, list_output)
