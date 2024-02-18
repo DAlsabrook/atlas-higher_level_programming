6626425811765bd2a02a166072ee9545c76c5766
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-17 18:52:42 -0600

    Removing server info

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates new user and grants all permissions to the user
 CREATE USER "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd" IF NULL;
-GRANT ALL ON hbtn_0c_0.* TO "user_0d_1"@"localhost":
+GRANT ALL TO "user_0d_1"@"localhost":
 FLUSH PRIVILEGES;
