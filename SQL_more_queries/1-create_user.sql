fe8ce513164eed837f90a602762a49dfe9be6d7f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-17 18:54:31 -0600

    Changing " to '

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates new user and grants all permissions to the user
-CREATE USER "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd" IF NULL;
-GRANT ALL TO "user_0d_1"@"localhost":
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NULL;
+GRANT ALL TO 'user_0d_1'@'localhost':
 FLUSH PRIVILEGES;
