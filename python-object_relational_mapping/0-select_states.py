b229b5030d29862a9fe09fdc912f758d70a2b320
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-19 10:50:36 -0600

    getting rid of _mysql and testing again

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 """
 This is a module to import a database and work with it
 """
-from MySQLdb import _mysql
+from MySQLdb
 import sys
 
 
@@ -11,12 +11,11 @@ def sql_connect(user, pw, db_name):
     This is a method to import a database
     """
 
-    print("Inside method")
-    db=_mysql.connect(host="localhost",
+    db = MySQLdb.connect(host="localhost",
                         port=3306,
                         password= pw,
                         database= db_name)
-    print(f"db = {db}")
+    print(f"Still connected: {db}")
 
 if __name__ == "__main__":
     sql_connect(sys.argv[1], sys.argv[2], sys.argv[3])
