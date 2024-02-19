1462d687a99d5eae2db10d6fcc4b87640a8dd14e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-18 21:42:20 -0600

    Shows with no genre attempt

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,13 @@
+-- Lists all shows contained in database without the genre linked
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres
+ON
+    tv_shows.id != tv_show_genres.show_id
+ORDER BY
+tv_shows.title ASC,
+tv_show_genres.genre_id ASC
