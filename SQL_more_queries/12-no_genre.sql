188164b597a76ba3a70d92d166a88c54cd58995b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-18 21:46:12 -0600

    Adding a where to find if genre id is null

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,13 +1,8 @@
 -- Lists all shows contained in database without the genre linked
-SELECT
-    tv_shows.title,
-    tv_show_genres.genre_id
-FROM
-    tv_shows
-LEFT JOIN
-    tv_show_genres
-ON
-    tv_shows.id != tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY
 tv_shows.title ASC,
 tv_show_genres.genre_id ASC
