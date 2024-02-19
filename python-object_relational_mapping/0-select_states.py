b2256d9710873e22a3fa4ec1003c5d21edcead14
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-19 11:09:34 -0600

    Accessing database and states table to get name and id

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,7 @@ def sql_connect(user, pw, db_name):
                         passwd= pw,
                         database= db_name)
     cur = db.cursor()
-    cur.execute("SHOW DATABASES")
+    cur.execute("USE hbtn_0e_0_usa SELECT id, name FROM states")
 
 if __name__ == "__main__":
     sql_connect(sys.argv[1], sys.argv[2], sys.argv[3])
