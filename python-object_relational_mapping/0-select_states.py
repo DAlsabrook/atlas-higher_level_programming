43554264e851f19deb6335da98871cbb69d9aed6
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-19 10:38:43 -0600

    Adding documentation to pass checker requirements

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from MySQLdb import _mysql
 import sys
+"""This is a module to import a database and work with it
+"""
 
 
 def sql_connect(user, pw, db_name):
+    """This is a method to import a database"""
 
     db=_mysql.connect(host="localhost",
                         port=3306,
