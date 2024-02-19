8c0a5c21562fc20eda1a62c7ac5dbf7354053615
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-18 22:22:40 -0600

    Changed to left join so it got all shows even if The have no genre

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all shows and genres linked to that show
 SELECT s.title, g.name
 FROM tv_shows s
-JOIN tv_show_genres sg ON s.id = sg.show_id
-JOIN tv_genres g ON sg.genre_id = g.id
+LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
 ORDER BY s.title ASC, g.name;
