f3c5fddb4358e1546399c0545ddc7a0a41efafc2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-19 10:45:20 -0600

    Another print for debug

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,13 +10,13 @@ def sql_connect(user, pw, db_name):
     """
     This is a method to import a database
     """
-    
+
     print("Inside method")
     db=_mysql.connect(host="localhost",
                         port=3306,
                         password= pw,
                         database= db_name)
-    print("Connected")
+    print(f"db = {db}")
 
 if __name__ == "__main__":
     sql_connect(sys.argv[1], sys.argv[2], sys.argv[3])
