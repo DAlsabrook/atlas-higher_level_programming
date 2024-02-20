91377b5bd44ce29e02262591cbe00bce7dac9dbd
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-20 15:14:22 -0600

    Removed spaces around name addition for pycodestyle

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     DBsession = sessionmaker(bind=engine)
     session = DBsession()
 
-    new_state = State(name = "Louisiana")
+    new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
     print(new_state.id)
