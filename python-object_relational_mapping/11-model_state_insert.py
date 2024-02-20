7e1f4553f6e1dfb9196a15a1cf73d2ca71bae3a7
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-20 15:10:30 -0600

    First attempt at adding an object to the database

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Module to create a new State object and add it to the database
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbase = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbase}'
+        )
+    Base.metadata.bind = engine
+    DBsession = sessionmaker(bind=engine)
+    session = DBsession()
+
+    new_state = State(name = "Louisiana")
+    session.add(new_state)
+    session.commit()
+    session.close()
