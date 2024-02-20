8ff9cc79ea008afa48e49449606a2cdcf1df30a9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-20 14:17:12 -0600

    First attempt at using filter in query and .contains to find an a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+Module to list all "state" objects that contain the char "a"
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbase = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbase}'
+        )
+    Base.metadata.bind = engine
+    DBsession = sessionmaker(bind=engine)
+    session = DBsession()
+
+    rows = session.query(State).filter(State.name.contians("a")).all()
+
+    for row in rows:
+        print(f"{row.id}: {row.name}")
+
+    session.close()
