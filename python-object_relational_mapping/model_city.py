d9180b9752fa363ddf05a3f4f8b25be2c2b1262e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-20 10:33:03 -0600

    First couple lines of state class. Not sure if needs Base.metadata.create all because that is in the model_state.py file

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,9 +2,14 @@
 """
 Module to contain class of a state
 """
-import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 class State(Base):
-    """State definition
-    """
-    
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
