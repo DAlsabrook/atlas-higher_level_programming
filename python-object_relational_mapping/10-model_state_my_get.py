922979e684fbcf849e6622acc96c1bb125005371
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-20 14:47:22 -0600

    First attempt at 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+module to search database for a specific name
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbase = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbase}'
+        )
+    Base.metadata.bind = engine
+    DBsession = sessionmaker(bind=engine)
+    session = DBsession()
+
+    rows=session.query(State).filter(State.name.ilike(f'%{state_name}%')).all()
+
+    for row in rows:
+        print(f"{row.id}: {row.name}")
+
+    session.close()
