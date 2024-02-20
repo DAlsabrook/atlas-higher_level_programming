5de5d36f3e88aca4b144aaf02345895cbe991497
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-20 17:49:37 -0600

    First attempt at task 12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Module to update the name of a state in the database
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbase = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbase}'
+        )
+    Base.metadata.bind = engine
+    DBsession = sessionmaker(bind=engine)
+    session = DBsession()
+
+    session.query(State).filter(State.id == 2).update({'name': 'New Mexico'})
+    session.commit()
+    session.close()
