3699e3bfd59f7648710f31a449b7f030be55d42e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-20 17:57:10 -0600

    First attempt to delete specific rows

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Module to delete all objects containing "a"
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbase = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbase}'
+    )
+    Base.metadata.bind = engine
+    DBsession = sessionmaker(bind=engine)
+    session = DBsession()
+
+    session.delete(State).filter(State.name.ilike("%a%"))
+
+    session.commit()
+    session.close()
