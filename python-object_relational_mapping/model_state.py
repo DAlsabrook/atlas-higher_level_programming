213fe31383602a4960fed16cea4dc592210dba04
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-20 10:35:17 -0600

    Wrote in wrong file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Module to contain class of a state
+"""
+from SQLAlchemy import Column, Integer, String
+from SQLAlchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
