051d7e7d6fb7025fbfdb1a362e71fde6c5b7f6e3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-24 03:12:57 +0000

    fix to semistandard

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const langs = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for(let i = 0; i < langs.length; i++){
-    console.log(langs[i])
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
 }
