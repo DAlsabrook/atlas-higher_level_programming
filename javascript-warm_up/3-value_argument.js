2a61b1684dd87eaa1bac73fc3de1ef3f9886b62e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-24 03:50:59 +0000

    first attempt at task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+let arg = 0;
+for (arg in args) {
+  if (arg > 1) {
+    console.log(args[arg]);
+  }
+}
+if (arg < 2) {
+  console.log('No argument');
+}
