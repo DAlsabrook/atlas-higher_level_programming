4b90d49e9501c624c7ccd1f4f83216bb46f81108
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 15:41:21 +0000

    First attempt at task 4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+
+if (!args[2]) {
+  args[2] = "undefined"
+}
+if (!args[3]) {
+  args[3] = "undefined"
+}
+if (args.length === 4) {
+  console.log(args[2] + " is " + args[3])
+}
