bbd222c3c743735f1438c230e847547e955bfb6c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 16:55:41 +0000

    first attempt at task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+
+if (isNaN(args[2])) {
+  console.log('Not a number');
+  return;
+}
+num = parseInt(args[2]);
+console.log('My number: ' + num);
