d0be397fdd5ee4d3f2312d0ef4d0ea693c1bc79e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 17:11:39 +0000

    single quote string

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const args = process.argv;
 const strToPrint = 'C is fun';
 
 if (isNaN(args[2])) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < args[2]; i++) {
     console.log(strToPrint);
