9a3a6d46c2cb2368195d225cf347f84b433e6770
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 18:39:33 +0000

    First attempt at task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+if (!args[0]) {
+  args[0] = 0;
+}
+let largestNum = -Infinity;
+let secondLargest = -Infinity;
+for (const idx in args) {
+  if (args[idx] > largestNum) {
+    secondLargest = largestNum;
+    largestNum = args[idx];
+  }
+}
+if (args.length === 1) {
+  console.log('0');
+} else {
+  console.log(secondLargest);
+}
