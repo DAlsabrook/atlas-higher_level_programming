692abaeca9173b6a02cb54570763f3d3272421e6
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 17:09:07 +0000

    First attempt at task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+const str_to_print = 'C is fun';
+
+if (isNaN(args[2])) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < args[2]; i++) {
+    console.log(str_to_print);
+  }
+}
