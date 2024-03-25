0bc5a4c2f8a1671979814bc866089ddd3bcf4e72
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 19:27:05 +0000

    Removing if statements to check for positive numbers

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,12 +2,8 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w > 0) {
-      this.width = w;
-    }
-    if (h > 0) {
-      this.height = h;
-    }
+    this.width = w;
+    this.height = h;
   }
 }
 
