607a1aa593d6393d2f68bf24e38cd57d0b1d5ccf
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 17:59:36 +0000

    First attempt at task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+function factorial (fact) {
+  if (fact < 0) {
+    return -1;
+  } else if (fact === 0) {
+    return 1;
+  } else {
+    return (fact * factorial(fact - 1));
+  }
+}
+
+const args = process.argv.slice(2);
+if (!args[0]) {
+  args[0] = 0;
+}
+const fact = parseInt(args[0]);
+console.log(factorial(fact));
