7d7f2fdd56ec3be40701be97c9145f2d8dfa8231
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 17:24:35 +0000

    First attempt at task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  const strToPrint = 'X'.repeat(size);
+  for (let row = 0; row < size; row++) {
+    console.log(strToPrint);
+  }
+}
