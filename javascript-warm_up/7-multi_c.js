9d554102f25ff162624c7aaa16cc09f221a6238c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 17:10:47 +0000

    var to camelcase

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,12 +2,12 @@
 
 const process = require('process');
 const args = process.argv;
-const str_to_print = 'C is fun';
+const strToPrint = 'C is fun';
 
 if (isNaN(args[2])) {
   console.log("Missing number of occurrences");
 } else {
   for (let i = 0; i < args[2]; i++) {
-    console.log(str_to_print);
+    console.log(strToPrint);
   }
 }
