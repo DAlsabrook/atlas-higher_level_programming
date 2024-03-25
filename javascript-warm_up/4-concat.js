9b6f38fd09afaf3431607ee4ba56e221116fa342
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-25 15:42:54 +0000

    fixing formatting

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -4,11 +4,11 @@ const process = require('process');
 const args = process.argv;
 
 if (!args[2]) {
-  args[2] = "undefined"
+  args[2] = 'undefined';
 }
 if (!args[3]) {
-  args[3] = "undefined"
+  args[3] = 'undefined';
 }
 if (args.length === 4) {
-  console.log(args[2] + " is " + args[3])
+  console.log(args[2] + ' is ' + args[3]);
 }
