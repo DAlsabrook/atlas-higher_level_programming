14d615b4a6605a3b3e862b28aa18976c3cb47c93
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-26 17:39:04 +0000

    First attempt at task 8

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const reversedList = [];
+  for (const element in list) {
+    reversedList.unshift(list[element]);
+  }
+  return reversedList;
+}
