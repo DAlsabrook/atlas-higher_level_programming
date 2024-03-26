0d628881f94aed02eebb98bd82fb3b74f40be044
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-26 18:00:57 +0000

    Changed from var to let

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-var functionCountLogMe = 0;
-exports.logMe = function (item) {
+let functionCountLogMe = 0;
+function logMe(item) {
   console.log(functionCountLogMe + ': ' + item);
   functionCountLogMe += 1;
 }
