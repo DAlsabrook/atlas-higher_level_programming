d86362ef9c169a9ae93882649b0edacc2feea1c8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-28 17:41:25 +0000

    Semistandard fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,15 +3,15 @@
 const process = require('process');
 const request = require('request');
 const args = process.argv;
-episodeNumber = args[2];
-baseUrl = "https://swapi-api.hbtn.io/api/films/";
+const episodeNumber = args[2];
+const baseUrl = 'https://swapi-api.hbtn.io/api/films/';
 
-if (episodeNumber)  {
-  api = baseUrl + episodeNumber;
+if (episodeNumber) {
+  const api = baseUrl + episodeNumber;
   request(api, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode === 200) {
       const data = JSON.parse(response.body);
       console.log(data.title);
     }
-  })
+  });
 }
