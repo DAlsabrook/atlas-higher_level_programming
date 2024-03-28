5dc1c08bf1b5fff3bbce96bad2cd9d8d3aa14eec
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-28 15:13:26 +0000

    First attempt at task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const fs = require("fs");
+process = require('process');
+args = process.argv;
+
+if (args[2]) {
+  file = args[2];
+  buffer = '';
+  fs.readFile(file, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.toString());
+    }
+    });
+}
